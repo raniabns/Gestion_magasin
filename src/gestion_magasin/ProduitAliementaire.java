a9261751064171e484f6b20615603ee1d4b17af1
raniabns <bensalemrania0@gmail.com>
2023-01-23 20:32:46 +0100

    afficher produit

---
+++ src/gestion_magasin/ProduitAliementaire.java
@@ -0,0 +1,77 @@
+package gestion_magasin;
+
+import java.util.Date;
+
+public class ProduitAliementaire {
+	  private int identifiant;
+	    private String libelle, marque;
+	    private float prix;
+	    private Date dateExpriration ; 
+	    
+	    
+
+	    public ProduitAliementaire() {
+	    }
+
+	    public ProduitAliementaire(int identifiant, String libelle, String marque) {
+	        this.identifiant = identifiant;
+	        this.libelle = libelle;
+	        this.marque = marque;
+	    }
+
+	    public ProduitAliementaire(int identifiant, String libelle, String marque, float prix) {
+	        this.identifiant = identifiant;
+	        this.libelle = libelle;
+	        this.marque = marque;
+	        this.prix = prix;
+	    }
+
+	    public int getIdentifiant() {
+	        return identifiant;
+	    }
+
+	    public void setIdentifiant(int identifiant) {
+	        this.identifiant = identifiant;
+	    }
+	    public String getLibelle() {
+	        return libelle;
+	    }
+
+	    public void setLibelle(String libelle) {
+	        this.libelle = libelle;
+	    }
+
+	    public String getMarque() {
+	        return marque;
+	    }
+
+	    public void setMarque(String marque) {
+	        this.marque = marque;
+	    }
+
+	    public float getPrix() {
+	        return prix;
+	    }
+
+	    public void setPrix(float prix) {
+	        this.prix = prix;
+	    }
+	    public Date getDateExpriration() {
+	        return dateExpriration;
+	    }
+
+	    public void setDateExpriration(Date dateExpriration) {
+	        this.dateExpriration = dateExpriration;
+	    }
+	    
+	    public void afficher(){
+	        System.out.println("------------------------");
+	        System.out.println("identifiant : "+this.identifiant);
+	        System.out.println("libelle : "+this.libelle);
+	        System.out.println("marque : "+this.marque);
+	        System.out.println("prix : "+this.prix);
+	        System.out.println("------------------------");
+	    }
+	    
+
+}
---
+++ src/gestion_magasin/TuniProdGestionMagasin.java
@@ -0,0 +1,15 @@
+package gestion_magasin;
+
+public class TuniProdGestionMagasin {
+	  public static void main(String[] args) {
+
+	        ProduitAliementaire produit1 = new ProduitAliementaire();
+	        ProduitAliementaire produit2 = new ProduitAliementaire(1021, "Lait", "Delice");
+	        ProduitAliementaire produit3 = new ProduitAliementaire(2510, "yaourt", "Vitalait");
+	        ProduitAliementaire produit4 = new ProduitAliementaire(3250, "Tomate", "Sicam", 1.200f);
+	        ProduitAliementaire produit5 = new ProduitAliementaire(3250, "Tomate", "Sicam", 1.200f);
+	        produit1.afficher();
+	        produit2.afficher();
+	        produit3.afficher();
+	        produit4.afficher();
+	  }}
