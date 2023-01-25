65ec951b0c784e8452a6ba68100082338d2d0752
omar skhiri <skhiri.mohamedomar@gmail.com>
2023-01-25 14:33:51 +0100

    ADD function toString to Display

--- src/gestion_magasin/Responsable.java
+++ src/gestion_magasin/Responsable.java
@@ -1,4 +1,14 @@
 package gestion_magasin;
-
+/**
+ * author : Mohamed Omar Skhiri **/
 public class Responsable {
+    float prime;
+
+    //Im just waiting for employee CLass
+
+
+    @Override
+    public String toString() {
+        return "Responsable{" + "prime=" + prime + '}' + " " + super.toString();
+    }
 }
