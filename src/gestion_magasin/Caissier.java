e6bb720175864786938f5230fb48aae5b68a6eea
mohamed ali ftouh <mohamedaliftouh@gmail.com>
2023-01-27 12:42:45 +0100

    update1 cassier classe

--- src/gestion_magasin/Caissier.java
+++ src/gestion_magasin/Caissier.java
@@ -1,5 +1,14 @@
 package gestion_magasin;
 
-public class Caissier {
+public class Caissier  extends Employe{
+    int numeroDeCaisse;
 
+    public Caissier(int numeroDeCaisse, int identifiant, String adresse, String nom, int nbrHeure) {
+        super(identifiant, adresse, nom, nbrHeure);
+        this.numeroDeCaisse = numeroDeCaisse;
+    }
+
+    
+    
+    
 }
