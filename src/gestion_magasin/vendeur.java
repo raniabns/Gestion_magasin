2725ff14e2423a231f65de3c46a982573fdb7ee4
kaouther88 <khalil.kaouther@esprit.tn>
2023-01-27 01:28:27 +0100

    commitjeudi

--- src/gestion_magasin/vendeur.java
+++ src/gestion_magasin/vendeur.java
@@ -2,5 +2,18 @@ package gestion_magasin;
 
 public class vendeur {
 	
+	package tn.tuniprod.gestionmagasin;
 
+	/**
+	 *
+	 * @author ACER
+	 */
+	public class Vendeur extends Employe {
+
+	    float tauxDeVente;
+
+	    public Vendeur(float tauxDeVente, int identifiant, String adresse, String nom, int nbrHeure) {
+	        super(identifiant, adresse, nom, nbrHeure);
+	        this.tauxDeVente = tauxDeVente;
+	    }
 }
