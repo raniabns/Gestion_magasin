fa37d94f591bf37ffec0ad9ae9e33fa3df1e49fa
kaouther88 <khalil.kaouther@esprit.tn>
2023-02-03 18:24:00 +0100

    final1commit

--- src/gestion_magasin/vendeur.java
+++ src/gestion_magasin/vendeur.java
@@ -1,29 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.tuniprod.gestionmagasin;
 
-	
-	package tn.tuniprod.gestionmagasin;
+/**
+ *
+ * @author ACER
+ */
+public class Vendeur extends Employe {
 
-	
-	public class Vendeur extends Employe {
+    float tauxDeVente;
 
-	    float tauxDeVente;
+    public Vendeur(float tauxDeVente, int identifiant, String adresse, String nom, int nbrHeure) {
+        super(identifiant, adresse, nom, nbrHeure);
+        this.tauxDeVente = tauxDeVente;
+    }
 
-	    public Vendeur(float tauxDeVente, int identifiant, String adresse, String nom, int nbrHeure) {
-	        super(identifiant, adresse, nom, nbrHeure);
-	        this.tauxDeVente = tauxDeVente;
-	    
+    @Override
+    public String toString() {
+        return "Vendeur{" + "tauxDeVente=" + tauxDeVente + '}' + " " + super.toString();
+    }
+
+    public float getSalaireEmploye() {
+        float salaire = 0;
+
+        salaire = (450 * (tauxDeVente / 100)) + 450;
+        
+        return salaire;
+    }
+
 }
-	    @Override
-	    public String toString() {
-	        return "Vendeur{" + "tauxDeVente=" + tauxDeVente + '}' + " " + super.toString();
-	    }
-	
-}
-	 public float getSalaireEmploye() {
-	        float salaire = 0;
-
-	        salaire = (450 * (tauxDeVente / 100)) + 450;
-	        
-	        return salaire;
-	    }
-
-	}
