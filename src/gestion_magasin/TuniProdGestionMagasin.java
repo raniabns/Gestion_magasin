0669892a085d48a500f0d834aba904945728daab
raniabns <bensalemrania0@gmail.com>
2023-02-03 20:37:16 +0100

    update class main tuniProdGestionMagasin

--- src/gestion_magasin/TuniProdGestionMagasin.java
+++ src/gestion_magasin/TuniProdGestionMagasin.java
@@ -2,7 +2,9 @@ package gestion_magasin;
 
 import gestion_magasin.Magasin;
 import gestion_magasin.ProduitAliementaire;
-import tn.tuniprod.gestionmagasin.Caissier;
+import gestion_magasin.Caissier;
+import gestion_magasin.Responsable;
+import gestion_magasin.Vendeur;
 
 public class TuniProdGestionMagasin {
 	  public static void main(String[] args) {
@@ -30,7 +32,28 @@ public class TuniProdGestionMagasin {
 	        Vendeur vendeurC1 = new Vendeur(10, 849895, "Charguia", "Nihel", 50);
 	        Caissier caissierC1 = new Caissier(5, 123456, "Ariana", "Mahamed", 36);
 	        Caissier caissierC2 = new Caissier(2, 894679, "Tunis", "Ali", 40);
-	  
-	  
-	  }}
+
+	        Caissier caissierM1 = new Caissier(9, 48912, "Tunis", "Kais", 42);
+	        Vendeur vendeurM1 = new Vendeur(15, 9485632, "Bizerte", "Abdou", 15);
+	        Responsable responsableM1 = new Responsable(1500, 9821421, "Ghazela", "Mouhaned", 170);
+	            System.out.println("SALAIRE :"+responsableM1.getSalaireEmploye());
+	        monoprix.ajouterEmploye(caissierC1);
+	        monoprix.ajouterEmploye(caissierC2);
+	        monoprix.ajouterEmploye(vendeurC1);
+	        carrefour.ajouterEmploye(caissierM1);
+	        carrefour.ajouterEmploye(vendeurM1);
+	        carrefour.ajouterEmploye(responsableM1);
+	       System.out.println("-------------");
+	        monoprix.ajouterProduit(produit2);
+	        monoprix.ajouterProduit(produit3);
+	        carrefour.ajouterProduit(produit3);
+	        carrefour.ajouterProduit(produit4);
+	        System.out.println("-------------");
+	        System.out.println(monoprix);
+	        System.out.println("-------------");
+	        System.out.println(carrefour);
+
+	    }
+
+	  }
 	     
