4f7c517b63fbb5cb1e151dc3abb8368cbffe1c5d
raniabns <bensalemrania0@gmail.com>
2023-02-04 10:04:19 +0100

    update TuniProd

--- src/gestion_magasin/TuniProdGestionMagasin.java
+++ src/gestion_magasin/TuniProdGestionMagasin.java
@@ -13,7 +13,8 @@ public class TuniProdGestionMagasin {
 	        ProduitAliementaire produit2 = new ProduitAliementaire(1021, "Lait", "Delice");
 	        ProduitAliementaire produit3 = new ProduitAliementaire(2510, "yaourt", "Vitalait");
 	        ProduitAliementaire produit4 = new ProduitAliementaire(3250, "Tomate", "Sicam", 1.200f);
-	        ProduitAliementaire produit5 = new ProduitAliementaire(3250, "Tomate", "Petit paris", 1.200f);
+	        ProduitAliementaire produit5 = new ProduitAliementaire(2250, "Tomate", "Petit paris", 1.200f);
+	   
 	        produit2.afficher();
 	        produit3.afficher();
 	        produit4.afficher();
